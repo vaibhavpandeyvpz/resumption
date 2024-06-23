dd984efba5e78a719cc737886f9f7b81e5ce2605
Vaibhav Pandey <contact@vaibhavpandey.com>
2024-06-23 17:50:24 +0530

    Clean up extra stuff.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
 FROM python:3.9-buster
 
-RUN apt-get update && \
-    apt-get install -y cron
-
 RUN pip install poetry==1.8.0
-
-RUN pip install poethepoet
 
 WORKDIR /app
 
@@ -15,7 +10,5 @@ COPY poetry.lock .
 RUN poetry install
 
 COPY . .
-
-RUN cat /app/crontab > /etc/crontab
 
 CMD ["poetry", "run", "uvicorn", "app.web:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/container.py
+++ app/container.py
@@ -1,16 +1,6 @@
 from dependency_injector.containers import DeclarativeContainer, WiringConfiguration
-from dependency_injector.providers import (
-    Callable,
-    Configuration,
-    Object,
-    Selector,
-    Singleton,
-)
-from drymail import SMTPMailer
+from dependency_injector.providers import Configuration
 from os import path
-from redis import Redis
-from rq import Queue
-from sqlalchemy import create_engine
 
 
 class Container(DeclarativeContainer):
@@ -21,47 +11,5 @@ class Container(DeclarativeContainer):
     )
 
     wiring_config = WiringConfiguration(
-        modules=[
-            ".commands",
-            ".jobs",
-            ".routers",
-        ],
-    )
-
-    engine = Singleton(
-        Callable(
-            create_engine,
-            config.db.url,
-        )
-    )
-
-    redis = Singleton(
-        Redis,
-        host=config.redis.host,
-        port=config.redis.port,
-    )
-
-    queue = Singleton(
-        Queue,
-        connection=redis,
-    )
-
-    mailer = Singleton(
-        SMTPMailer,
-        host=config.mail.host,
-        port=config.mail.port,
-        user=config.mail.username,
-        password=config.mail.password,
-        ssl=Selector(
-            config.mail.encryption,
-            ssl=Object(True),
-            tls=Object(False),
-            none=Object(False),
-        ),
-        tls=Selector(
-            config.mail.encryption,
-            ssl=Object(False),
-            tls=Object(True),
-            none=Object(False),
-        ),
+        modules=[".routers.index"],
     )
